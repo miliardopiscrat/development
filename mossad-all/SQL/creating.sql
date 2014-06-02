4fd4a41e0755147a35b0aa247c1ec9ac9a1054f3
kisonar <kisonar@wp.pl>
2014-06-02 16:58:17 +0200

    Updates

--- mossad-all/SQL/creating.sql
+++ mossad-all/SQL/creating.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS `task_status` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `val` varchar(80) COLLATE utf8_polish_ci NOT NULL,
+  `val` varchar(20) COLLATE utf8_polish_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=1 ;
 
 CREATE TABLE IF NOT EXISTS `task_type` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `val` varchar(80) COLLATE utf8_polish_ci NOT NULL,
+  `val` varchar(20) COLLATE utf8_polish_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=1 ;
 
@@ -16,17 +16,15 @@ CREATE TABLE IF NOT EXISTS `task_priority` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=1 ;
 
-
 CREATE TABLE IF NOT EXISTS `mossad_user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` text,
   `surname` text,
   `email` text NOT NULL,  
-  `password` text NOT NULL,
-  
-  PRIMARY KEY (`id`)
+  `password` text NOT NULL, 
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uk-email` (`email`(10))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=1 ;
-
 
 CREATE TABLE IF NOT EXISTS `mossad_task` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -39,8 +37,11 @@ CREATE TABLE IF NOT EXISTS `mossad_task` (
   PRIMARY KEY (`id`),
   CONSTRAINT `priority_fk` FOREIGN KEY (`priority`) REFERENCES `task_priority` (`id`),
   CONSTRAINT `status_fk` FOREIGN KEY (`status`) REFERENCES `task_status` (`id`), 
-  CONSTRAINT `type_fk` FOREIGN KEY (`type`) REFERENCES `task_type` (`id`)
+  CONSTRAINT `type_fk` FOREIGN KEY (`type`) REFERENCES `task_type` (`id`),
+  CONSTRAINT `user_fk` FOREIGN KEY (`userId`) REFERENCES `mossad_user` (`id`)
+  
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=1 ;
+
 
 
 insert into `task_status` values (1,'Not started');
@@ -63,8 +64,25 @@ insert into `task_priority` values (3,'Low');
 insert into `task_priority` values (4,'Not specified');
 
 insert into `mossad_user` values (1,'user1name','user1surname','user1email@wp.pl','user1password');
+insert into `mossad_user` values (2,'user2name','user2surname','user2email@wp.pl','user2password');
 
---//TODO to add to task table
+INSERT INTO `mossad_task` (`id`, `title`, `description`, `priority`, `userId`, `status`, `type`) VALUES ('1', 'Task title', 'Task descrption', '2', '1', '3', '6');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--TODO to add to task table dodac pozniuej
 --due time
 --repetition
 --estimation how long
